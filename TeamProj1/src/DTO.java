62a1afb0da2a02b499cb49cdc875bdacc28168a1
admin <admin@admin.com>
2019-03-12 12:06:44 +0900

    학우너에서 두번째 파일생성 및 수정

---
+++ TeamProj1/src/DTO.java
@@ -0,0 +1,18 @@
+
+public class DTO {
+	private String name;
+	public DTO(String name) {
+		super();
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	
+}
